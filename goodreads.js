6f6628672bcf76197a87b8a8bd20f762b4fde6c5
ngntrgduc <trungducnguyen1511@gmail.com>
2024-12-06 21:23:58 +0700

    Update script

--- goodreads.js
+++ goodreads.js
@@ -1,31 +1,45 @@
-function hideUnwantedLanguageOptions() {
-    const filterBoard = document.getElementsByClassName('ReviewFiltersModal')[0];
-    const radioGroups = filterBoard.getElementsByClassName('RadioGroup');
-    const languageFilter = radioGroups[radioGroups.length - 1];
-    const divs = languageFilter.getElementsByClassName('RadioGroup__input');
+// ==UserScript==
+// @name         Goodreads hide language options
+// @version      0.1
+// @description  Hide unwanted language options
+// @author       ngntrgduc
+// @match        https://www.goodreads.com/book/show/*
+// @grant        none
+// ==/UserScript==
 
-    Array.from(divs).forEach(div => {
-        const label = div.querySelector('.RadioInput'); // Get the nested label
-        if (!label) return;
-        const labelText = label.textContent.trim();
-        if (!labelText.includes("English") && !labelText.includes("Tiếng Việt")) {
-            div.style.display = 'none';    
-        }
-    });
-}
+(function() {
+    'use strict';
 
-// Set up a MutationObserver to detect when the overlay appears in the DOM
-const observer = new MutationObserver((mutationsList) => {
-    for (let mutation of mutationsList) {
-        if (mutation.type === 'childList') {
-            const overlay = document.querySelector('.Overlay.Overlay--floating');
-            if (overlay) {
-                hideUnwantedLanguageOptions();
-                break;
-            }
-        }
-    }
-});
+	function hideUnwantedLanguageOptions() {
+		const filterBoard = document.getElementsByClassName('ReviewFiltersModal')[0];
+		const radioGroups = filterBoard.getElementsByClassName('RadioGroup');
+		const languageFilter = radioGroups[radioGroups.length - 1];
+		const divs = languageFilter.getElementsByClassName('RadioGroup__input');
 
-// Start observing the document body for added nodes
-observer.observe(document.body, { childList: true, subtree: true });
+		Array.from(divs).forEach(div => {
+			const label = div.querySelector('.RadioInput'); // Get the nested label
+			if (!label) return;
+			const labelText = label.textContent.trim();
+			if (!labelText.includes("English") && !labelText.includes("Tiếng Việt")) {
+				div.style.display = 'none';
+			}
+		});
+	}
+
+	// Set up a MutationObserver to detect when the overlay appears in the DOM
+	const observer = new MutationObserver((mutationsList) => {
+		for (let mutation of mutationsList) {
+			if (mutation.type === 'childList') {
+				const overlay = document.querySelector('.Overlay.Overlay--floating');
+				if (overlay) {
+					hideUnwantedLanguageOptions();
+					break;
+				}
+			}
+		}
+	});
+
+	// Start observing the document body for added nodes
+	observer.observe(document.body, { childList: true, subtree: true });
+
+})();
